c3f6de0cbb1f2721e043b6e4e2711c3f4c6d7bc4
arcanlte <pinangayjoel@gmail.com>
2020-06-08 10:34:00 +0800

    Testing

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [outputText, setOutputText] = useState("This text");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Text>...</Text>
+      </View>
+      <View>
+        <Text>...</Text>
+      </View>
     </View>
   );
 }
@@ -12,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
